eb6a529fcd685cfd4d8610606aad43342c6ca64b
luo dingli <godofpen05@gmail.com>
2021-03-13 13:16:16 +0900

    Add all modified files

---
+++ Plugins/TemaranShaderTutorial/Source/ShaderDeclarationDemo/Private/DynamicPositionVertexBuffer.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DynamicPositionVertexBuffer.h"
+
+
+FDynamicPositionVertexBuffer::FDynamicPositionVertexBuffer()
+	: FVertexBufferWithSRV()
+{
+}
+
+FDynamicPositionVertexBuffer::~FDynamicPositionVertexBuffer()
+{
+}
+
+void FDynamicPositionVertexBuffer::CleanUp()
+{
+}
+
+void FDynamicPositionVertexBuffer::Init(uint32 InNumVertices)
+{
+	this->NumVertices = InNumVertices;
+}
+
+void FDynamicPositionVertexBuffer::InitRHI()
+{
+	FVertexBufferWithSRV::InitRHI();
+	if (NumVertices)
+	{
+		FRHIResourceCreateInfo CreateInfo;
+		uint32 SizeInBytes = NumVertices * sizeof(FPositionVertex);
+		this->VertexBufferRHI = RHICreateVertexBuffer(
+			SizeInBytes,
+			 BUF_ShaderResource | BUF_UnorderedAccess | BUF_SourceCopy | BUF_ByteAddressBuffer,
+			CreateInfo
+		);
+		this->ShaderResourceViewRHI = RHICreateShaderResourceView(
+			FShaderResourceViewInitializer(VertexBufferRHI,PF_R32_FLOAT )
+		);
+		this->UnorderedAccessViewRHI = RHICreateUnorderedAccessView(VertexBufferRHI, PF_R32_FLOAT);
+	}
+}
+
+void FDynamicPositionVertexBuffer::ReleaseRHI()
+{
+	FVertexBufferWithSRV::ReleaseRHI();
+}
+
+void FDynamicPositionVertexBuffer::BindPositionVertexBuffer(const FVertexFactory* VertexFactory,
+	FStaticMeshDataType& StaticMeshData)
+{
+	StaticMeshData.PositionComponent = FVertexStreamComponent(
+        this,
+        STRUCT_OFFSET(FPositionVertex, Position),
+        sizeof(FPositionVertex),
+        VET_Float3
+    );
+	StaticMeshData.PositionComponentSRV = ShaderResourceViewRHI;
+}
---
+++ Plugins/TemaranShaderTutorial/Source/ShaderUsageDemo/Private/GPUMeshModifier.cpp
@@ -0,0 +1,76 @@
+﻿#include "GPUMeshModifier.h"
+
+#include "ShaderDeclarationDemoModule.h"
+
+AGPUMeshModifier::AGPUMeshModifier()
+	:Super()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+}
+
+void AGPUMeshModifier::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+    if (MetaballCenters.Num() == 0)
+    {
+        return;
+    }
+
+    
+    FGPUMeshParameters GPUMeshParameters = FShaderDeclarationDemoModule::Get().GetGPUMeshParameters();
+    GPUMeshParameters.SDFTexture = this->GPUMeshSDFTexture ;
+    GPUMeshParameters.SDFTextureSize = FIntVector(
+        this->GPUMeshSDFTexture->GetSizeX(),
+        this->GPUMeshSDFTexture->GetSizeY(),
+        this->GPUMeshSDFTexture->GetSizeZ()
+        );
+    GPUMeshParameters.BrushList.Empty();
+    for (FVector center : MetaballCenters)
+    {
+        GPUMeshParameters.BrushList.Add(
+            FGPUMeshParameters::FBrushParameter
+            {
+                center,
+                MetaBallRadius
+            }
+        );
+    }
+    FShaderDeclarationDemoModule::Get().UpdateGPUMeshParameters(GPUMeshParameters);
+
+}
+
+void AGPUMeshModifier::BeginPlay()
+{
+	Super::BeginPlay();
+    if (this->MetaballCenters.Num() == 0)
+    {
+        return;
+    }
+// 	this->GPUMeshSDFTexture->UpdateSourceFromFunction(
+// [=](int x, int y, int z, void* output)
+//     {
+//         FVector pos = FVector(x,y,z);
+//         bool isInside = false;
+//         for (FVector center : MetaballCenters)
+//         {
+//             float distance = FVector::Distance(center / 100.0f,pos);
+//             if ( distance < MetaBallRadius)
+//             {
+//                 isInside = true;
+//             }
+//         }
+//         if(isInside)
+//         {
+//             *((FFloat16*)output) = FFloat16(1.0f);
+//         }else
+//         {
+//             *((FFloat16*)output) = FFloat16(0.0f);
+//         }
+//     },
+//     GPUMeshSDFTexture->GetSizeX(),
+//     GPUMeshSDFTexture->GetSizeY(),
+//     GPUMeshSDFTexture->GetSizeZ(),
+//     ETextureSourceFormat::TSF_G16
+//);
+}
